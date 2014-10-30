9fed0864838e4be0dc134ad163235711037c745d
Raul Ramirez <raul287986@gmail.com>
2014-10-30 08:53:09 -0700

    this is my databse that i saved to phpmyadmin

---
+++ controller/create-db.php
@@ -0,0 +1,25 @@
+<?php
+    require_once(__DIR__ . "/../model/database.php");
+
+    $connection = new mysqli($host, $username, $password);
+    
+    if($connection->connect_error) {
+        die("Reeor: " . $connection->connect_error); 
+    }
+    else {
+        echo "Success" . $connection->host_info;
+    }
+    $exists = $connection->select_db($database);
+    
+    if(!$exists) {
+     $query = $connection->query("CREATE DATABASE $database");
+     
+     if($query) {
+         echo "Successfully created database:" . $database;
+     }
+    }
+    else {
+        echo "Database already exists"
+    }
+    
+    $connection->close();
