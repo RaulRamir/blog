dd214bb0d836c1adf15d495652a0ff10f47b2fff
Raul Ramirez <raul287986@gmail.com>
2014-11-18 09:12:49 -0800

    made an error connection killer

--- model/Database.php
+++ model/Database.php
@@ -1,28 +1,35 @@
 <?php
+
 class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
-    
+
     public function __construct($host, $username, $password, $database) {
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
     }
-    
+
     public function openConnection() {
-        
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($connection->connect_error) {
+            die("<p>Reeor: " . $this->connection->connect_error . "</p>");
+            //kills the connection when theirs is an error in the connection
+        }
     }
-    
+
     public function closeConnection() {
         
     }
-    
+
     public function query($string) {
         //create an new object and call it specifically on this function 
-        
     }
+
 }
